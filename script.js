b174437f8a2f357e56034cbd0e9f7871c60f37d6
kou1gn <2223122@student.creativehk.edu.hk>
2026-03-10 15:02:22 +0800

    actually added stuff

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('Hello!');
+function triggerUpload() {
+    document.getElementById('image-input').click();
+  }
+  
+  function updateImage(input) {
+    if (input.files && input.files[0]) {
+      const reader = new FileReader();
+      
+      reader.onload = function(e) {
+        document.getElementById('avatar-preview').src = e.target.result;
+      };
+      
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
